9604d41731fb61270598a3a24c9749c9cbb193c8
juanmanujr <161176209+juanmanujr@users.noreply.github.com>
2026-05-26 23:30:11 -0300

    Add files via upload

---
+++ WindowsApp5/WindowsApp5/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim numero1 As Double
+        Dim numero2 As Double
+        Dim resultado As Double
+
+        If Double.TryParse(TextBox1.Text, numero1) AndAlso Double.TryParse(TextBox2.Text, numero2) Then
+
+            resultado = numero1 + numero2
+
+            Label1.Text = "Resultado: " & resultado.ToString()
+
+        Else
+
+            MessageBox.Show("Ingrese valores numéricos válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End If
+    End Sub
+End Class
---
+++ WindowsApp6/WindowsApp6/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Security.Cryptography
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim random As New Random()
+        Dim rojo As Integer = random.Next(0, 256)
+        Dim verde As Integer = random.Next(0, 256)
+        Dim azul As Integer = random.Next(0, 256)
+
+        Me.BackColor = Color.FromArgb(rojo, verde, azul)
+
+
+    End Sub
+End Class
