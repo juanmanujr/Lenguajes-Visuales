c3a32e7b685ea873d32db14b4088b0e6940e7097
juanmanujr <161176209+juanmanujr@users.noreply.github.com>
2026-05-27 00:26:52 -0300

    Add files via upload

---
+++ conexion/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.txtCi = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnInsertar = New System.Windows.Forms.Button()
+        Me.dgvClientes = New System.Windows.Forms.DataGridView()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        CType(Me.dgvClientes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(158, 32)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 22)
+        Me.txtID.TabIndex = 0
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(158, 87)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 22)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(158, 152)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(100, 22)
+        Me.txtApellido.TabIndex = 2
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(158, 214)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 22)
+        Me.txtTelefono.TabIndex = 3
+        '
+        'txtCi
+        '
+        Me.txtCi.Location = New System.Drawing.Point(158, 274)
+        Me.txtCi.Name = "txtCi"
+        Me.txtCi.Size = New System.Drawing.Size(100, 22)
+        Me.txtCi.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "ID"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(31, 155)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label3.Size = New System.Drawing.Size(58, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Apellido"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Telefono"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(31, 277)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(20, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Ci"
+        '
+        'btnInsertar
+        '
+        Me.btnInsertar.Location = New System.Drawing.Point(12, 339)
+        Me.btnInsertar.Name = "btnInsertar"
+        Me.btnInsertar.Size = New System.Drawing.Size(141, 53)
+        Me.btnInsertar.TabIndex = 10
+        Me.btnInsertar.Text = "Insertar Cliente"
+        Me.btnInsertar.UseVisualStyleBackColor = True
+        '
+        'dgvClientes
+        '
+        Me.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvClientes.Location = New System.Drawing.Point(368, 30)
+        Me.dgvClientes.Name = "dgvClientes"
+        Me.dgvClientes.RowHeadersWidth = 51
+        Me.dgvClientes.Size = New System.Drawing.Size(385, 264)
+        Me.dgvClientes.TabIndex = 11
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(495, 339)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(145, 53)
+        Me.btnMostrar.TabIndex = 12
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(182, 339)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(156, 53)
+        Me.btnEliminar.TabIndex = 13
+        Me.btnEliminar.Text = "Eliminar Cliente"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Controls.Add(Me.dgvClientes)
+        Me.Controls.Add(Me.btnInsertar)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCi)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtID)
+        Me.Name = "Form1"
+        Me.Text = "Añadir Clientes"
+        CType(Me.dgvClientes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtCi As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnInsertar As Button
+    Friend WithEvents dgvClientes As DataGridView
+    Friend WithEvents btnMostrar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
---
+++ conexion/Form1.vb
@@ -0,0 +1,119 @@
+﻿Imports Npgsql
+
+Public Class Form1
+    Private Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+        Dim connString As String = "Host=localhost;Port=5432;Username=postgres;Password=Queteimporta9;Database=mi_base"
+        Dim conn As New NpgsqlConnection(connString)
+
+        Try
+            conn.Open()
+
+            Dim query As String = "INSERT INTO clientes(id, nombre, apellido, telefono, ci) VALUES (@id, @nombre, @apellido, @telefono, @ci)"
+            Dim cmd As New NpgsqlCommand(query, conn)
+
+            cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text))
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@apellido", txtApellido.Text)
+            cmd.Parameters.AddWithValue("@telefono", txtTelefono.Text)
+            cmd.Parameters.AddWithValue("@ci", txtCi.Text)
+
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Cliente guardado con éxito")
+            'para limpiar los campos de id,nombre, apellido,telefono,ci,id, despues de ingresar
+            txtID.Clear()
+            txtNombre.Clear()
+            txtApellido.Clear()
+            txtTelefono.Clear()
+            txtCi.Clear()
+            txtID.Focus()
+            conn.Close()
+
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        Dim connString As String = "Host=localhost;Port=5432;Username=postgres;Password=Queteimporta9;Database=mi_base"
+        Dim conn As New NpgsqlConnection(connString)
+
+        Try
+            'abrir la conexion con la base de datos
+            conn.Open()
+
+            Dim query As String = "SELECT *FROM clientes"
+            Dim cmd As New NpgsqlCommand(query, conn)
+            Dim da As New NpgsqlDataAdapter(cmd)
+            Dim dt As New DataTable()
+
+            da.Fill(dt)
+            dgvClientes.DataSource = dt
+
+            'cerrar la conexion con la base de datos
+            conn.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Error al mostrar clientes: " & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        'creacion de la llave para conectarse a la base de datos
+
+        Dim connString As String = "Host=localhost;Port=5432;Username=postgres;Password=Queteimporta9;Database=mi_base"
+        ' 'dimm connstring as string: crea una variable tipo texto llamada connstring
+        'lo de adentro es la cadena de conexion, que le dice al programa donde esta la base, con que usuario y que base usar
+        'host=localhost: la base de datos esta en nuestra propia computadora
+        'port=5432 es el puerto donde se conecta postgresql
+        'username=postgres: nuestro usuario de postgresql
+        'pasword=queteimporta: la contraseña para entrar
+        'database=mi_base el nombre de tu base de datos
+        'es como el gps para que el programa encuentre la base y pueda trabajar con ella
+        Dim conn As New NpgsqlConnection(connString)
+
+        Try
+            'abrir la conexion con la base de datos
+            conn.Open()
+
+            Dim query As String = "DELETE FROM clientes WHERE id = @id"
+            Dim cmd As New NpgsqlCommand(query, conn)
+
+
+
+
+            cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtID.Text)) 'preparar un dato para mandarle a la base de datos
+            'cmd: comando que ejecuta el insert
+            '.parameter.add.whitvalue: decirle el valor que vas a usar en el campo @id
+            '@id nombre del parametro en la consulta sql
+            'convert.toint32(txtid.text) agarra el texto del id y lo convierte en numero
+
+
+            Dim result As Integer = cmd.ExecuteNonQuery()
+            'cmd.executenonquery() ejecuta el comando sql
+            If result > 0 Then
+                MessageBox.Show("Cliente eliminado correctamente")
+            Else
+                MessageBox.Show("No se encontró un cliente con ese ID")
+            End If
+
+
+            conn.Close()
+            btnMostrar.PerformClick() 'actualiza la tabla automaticamente
+            'btnMostrar tiene codigo que actualiza la tabla, entonces .perfomclick se ejecuta sin hacer click en btnMostrar 
+            'asi la tabla se actualiza sola
+        Catch ex As Exception
+            MessageBox.Show("Error al eliminar cliente: " & ex.Message)
+
+        End Try
+
+
+
+
+
+
+    End Sub
+End Class
