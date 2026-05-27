f70f0be25f435d3d8f9284865882179edfc30eab
juanmanujr <161176209+juanmanujr@users.noreply.github.com>
2026-05-26 23:22:43 -0300

    Add files via upload

---
+++ mostrar/mostrar/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMostrar = New System.Windows.Forms.Button()
+        Me.txtHola = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnMostrar
+        '
+        Me.btnMostrar.Location = New System.Drawing.Point(144, 124)
+        Me.btnMostrar.Name = "btnMostrar"
+        Me.btnMostrar.Size = New System.Drawing.Size(75, 32)
+        Me.btnMostrar.TabIndex = 0
+        Me.btnMostrar.Text = "Mostrar"
+        Me.btnMostrar.UseVisualStyleBackColor = True
+        '
+        'txtHola
+        '
+        Me.txtHola.Location = New System.Drawing.Point(286, 129)
+        Me.txtHola.Name = "txtHola"
+        Me.txtHola.Size = New System.Drawing.Size(100, 22)
+        Me.txtHola.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(601, 328)
+        Me.Controls.Add(Me.txtHola)
+        Me.Controls.Add(Me.btnMostrar)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frmMostrar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnMostrar As Button
+    Friend WithEvents txtHola As TextBox
+End Class
---
+++ mostrar/mostrar/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+
+    Private Sub btnMostrar_Click(sender As Object, e As EventArgs) Handles btnMostrar.Click
+        txtHola.Text = "Hola perra"
+    End Sub
+End Class
