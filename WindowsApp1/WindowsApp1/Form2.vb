e50e1dd2202648c1e3ad42e4770038d86ca763f1
juanmanujr <161176209+juanmanujr@users.noreply.github.com>
2026-05-26 23:34:30 -0300

    Add files via upload

---
+++ WindowsApp1/WindowsApp1/Form2.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_DpiChangedAfterParent(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
